b0b3cda0ef47d9ddb70b86004347c643a91a1e04
Yeison Danner carhuapoma Dett <51946409+yeisondanner@users.noreply.github.com>
2025-11-08 16:07:02 -0500

    update view

--- Libraries/Core/Views.php
+++ Libraries/Core/Views.php
@@ -2,16 +2,25 @@
 
 class Views
 {
-	function getView($controller, $view, $data = "")
+	function getView($controller, $view, $data = "", string $type = "Admin")
 	{
 		$controller = get_class($controller);
-		if ($controller == "Home") {
-			$view = "Views/" . $view . ".php";
+		if ($type === "Admin") {
+			if ($controller == "Home") {
+				$view = "Views/" . $view . ".php";
+			} else {
+				$view = "Views/App/Admin/" . $controller . "/" . $view . ".php";
+			}
+		} else if ($type === "POS") {
+			if ($controller == "Home") {
+				$view = "Views/" . $view . ".php";
+			} else {
+				$view = "Views/App/POS/" . $controller . "/" . $view . ".php";
+			}
 		} else {
-			$view = "Views/App/" . $controller . "/" . $view . ".php";
+			//aqui debe mostrar un vista de error
+			die();
 		}
 		require_once($view);
 	}
 }
-
-?>
