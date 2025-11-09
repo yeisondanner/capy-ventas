f3c3c4f03af65c1e6237a3041e653c907090b73c
unknown <jhonaltamiranoaltamirano28@gmail.com>
2025-11-08 23:50:05 -0500

    xvideos

--- Controllers/Admin/Businesstype.php
+++ Controllers/Admin/Businesstype.php
@@ -25,11 +25,11 @@ class Businesstype extends Controllers
      *
      * @return void
      */
-    public function Businesstype()
+    public function businesstype()
     {
         // Datos de la página (asignación única para claridad y mantenimiento)
         $data = [
-            'page_id'          => 66,
+            'page_id'          => 12,
             'page_title'       => 'Gestión de negocios',
             'page_description' => 'Gestiona todos los tipos de negocios.',
             'page_container'   => 'Businesstype',
@@ -73,4 +73,50 @@ class Businesstype extends Controllers
         // Render de la vista del módulo
         $this->views->getView($this, 'businesstype', $data);
     }
+    /**
+     * Funcion que desvuelve la lista de los roles a la vista
+     * @return void
+     */
+    public function getBusinesstype()
+    {
+        permissionInterface(12);
+        $arrData = $this->model->select_businesstype();
+        $cont = 1; //Contador para la tabla
+        foreach ($arrData as $key => $value) {
+            $arrData[$key]["cont"] = $cont;
+            if ($value["r_status"] == "Activo") {
+                $arrData[$key]["status"] = '<span class="badge badge-success"><i class="fa fa-check"></i> Activo</span>';
+            } else {
+                $arrData[$key]["status"] = '<span class="badge badge-danger"><i class="fa fa-close"></i> Inactivo</span>';
+            }
+            if ($value["idBusinessType"] != 1) {
+                $arrData[$key]["actions"] = '
+                 <div class="btn-group btn-group-sm" role="group">
+                <button class="btn btn-success update-item" data-id="' . $value["idBusinessType"] . '" data-name="' . $value["r_name"] . '" data-status="' . $value['r_status'] . '"  data-description="' . $value["r_description"] . '" type="button"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-info report-item" data-id="' . $value["idBusinessType"] . '" data-name="' . $value["r_name"] . '" data-status="' . $value['r_status'] . '"  data-description="' . $value["r_description"] . '" data-registrationDate="' . dateFormat($value['r_registrationDate']) . '" data-updateDate="' . dateFormat($value['r_updateDate']) . '" type="button"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></button>
+                  <a href="' . base_url() . '/pdf/rol/' . encryption($value['idBusinessType']) . '" target="_Blank" class="btn btn-warning btn-sm report-pdf">
+                        <i class="fa fa-print"></i>
+                    </a>
+                <button class="btn btn-secondary permission-item" data-id="' . $value["idBusinessType"] . '" data-name="' . $value["r_name"] . '"   data-description="' . $value["r_description"] . '" type="button"><i class="fa fa-th-list" aria-hidden="true"></i></button>
+                <button class="btn btn-danger delete-item" data-id="' . $value["idBusinessType"] . '" data-name="' . $value["r_name"] . '" ><i class="fa fa-remove"></i></button>
+                </div>
+                '; //Botones de acciones
+            } else {
+                $arrData[$key]["actions"] = ' 
+                 <div class="btn-group btn-group-sm" role="group">
+                <button class="btn btn-success update-item" data-id="' . $value["idBusinessType"] . '" data-name="' . $value["r_name"] . '" data-status="' . $value['r_status'] . '" data-description="' . $value["r_description"] . '" type="button"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-info report-item" data-id="' . $value["idBusinessType"] . '" data-name="' . $value["r_name"] . '" data-status="' . $value['r_status'] . '" data-description="' . $value["r_description"] . '" data-registrationDate="' . dateFormat($value['r_registrationDate']) . '" data-updateDate="' . dateFormat($value['r_updateDate']) . '"  type="button"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></button> 
+                 <a href="' . base_url() . '/pdf/rol/' . encryption($value['idBusinessType']) . '" target="_Blank" class="btn btn-warning btn-sm report-pdf">
+                        <i class="fa fa-print"></i>
+                    </a>
+                </div>';
+            }
+            $cont++;
+        }
+        toJson($arrData);
+    }
+    /**
+     * Funcion que permite el registro del usuario nuevo
+     * @return void
+     */
 }
--- Views/App/Admin/Businesstype/businesstype.php
+++ Views/App/Admin/Businesstype/businesstype.php
@@ -43,4 +43,54 @@
 </main>
 
 <?= footerAdmin($data) ?>
+<!-- Sección de Modals -->
+<!-- Modal Save -->
+<div class="modal fade" id="modalSave" tabindex="-1" role="dialog" aria-labelledby="modalSaveLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title" id="modalSaveLabel">Registro de Negocios</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Formulario de Registro de Roles -->
+                <div class="tile-body">
+                    <form id="formSave" enctype="multipart/form-data" autocomplete="off">
+                        <?= csrf(); ?>
 
+                        <div class="form-group">
+                            <label class="control-label" for="txtBusinesstypeName">Nombre
+                                <span class="text-danger">*</span>
+                            </label>
+                            <input class="form-control" type="text" id="txtBusinesstypeName" name="txtBusinesstypeName" required
+                                placeholder="Ingrese el nombre del tipo de negocio" maxlength="250" minlength="4"
+                                pattern="^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{4,250}$"
+                                title="El nombre debe contener entre 4 y 250 caracteres y solo incluir letras y espacios.">
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label" for="txtBusinesstypeName">Descripción
+                            </label>
+                            <textarea class="form-control" id="txtBusinesstypeName" name="txtBusinesstypeName"
+                                minlength="20" pattern="^[a-zA-ZÁÉÍÓÚáéíóúÜüÑñ0-9\s.,;:!?()-]+$"
+                                placeholder="Ingrese la descripción del tipo de negocio"></textarea>
+                            <small class="form-text text-muted">
+                                La descripción debe tener al menos 20 caracteres y solo puede incluir letras, números,
+                                espacios, guiones altos y bajos.
+                            </small>
+                        </div>
+                        <div class="d-flex justify-content-center mt-3">
+                            <button class="btn btn-primary btn-block" type="submit">
+                                <i class="fa fa-fw fa-lg fa-save"></i> Registrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+            </div>
+        </div>
+    </div>
+</div>
