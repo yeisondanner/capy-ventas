2d2b874b4a0eaf23b5f716bd34feafc700b33b52
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-11-12 00:13:00 -0500

    update de datos

--- Views/Template/panelPos/foot.php
+++ Views/Template/panelPos/foot.php
@@ -2,6 +2,18 @@
 <script src="<?= media() ?>/js/libraries/POS/jquery-3.7.0.min.js?<?= versionSystem() ?>"></script>
 <!-- TODO: boostrap.min.js -->
 <script src="<?= media() ?>/js/libraries/POS/bootstrap.min.js?<?= versionSystem() ?>"></script>
+<!-- Data table plugin-->
+<script type="text/javascript" src="<?= media() ?>/js/libraries/Admin/plugins/jquery.dataTables.min.js?<?= versionSystem() ?>"></script>
+<script type="text/javascript" src="<?= media() ?>/js/libraries/Admin/plugins/dataTables.bootstrap.min.js?<?= versionSystem() ?>"></script>
+
+<!-- Buttons for DataTables-->
+<script type="text/javascript" language="javascript"
+    src="<?= media() ?>/js/libraries/Admin/plugins/dataTables.buttons.min.js?<?= versionSystem() ?>"></script>
+<script type="text/javascript" language="javascript" src="<?= media() ?>/js/libraries/Admin/plugins/jszip.min.js?<?= versionSystem() ?>"></script>
+<script type="text/javascript" language="javascript" src="<?= media() ?>/js/libraries/Admin/plugins/pdfmake.min.js?<?= versionSystem() ?>"></script>
+<script type="text/javascript" language="javascript" src="<?= media() ?>/js/libraries/Admin/plugins/vfs_fonts.js?<?= versionSystem() ?>"></script>
+<script type="text/javascript" language="javascript"
+    src="<?= media() ?>/js/libraries/Admin/plugins/buttons.html5.min.js?<?= versionSystem() ?>"></script>
 <!-- TODO: libreria principal main-->
 <script src="<?= media() ?>/js/libraries/POS/main.js?<?= versionSystem() ?>"></script>
 
