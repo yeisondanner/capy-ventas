efee2cdfe7182080e6d76e00ecd0b22e19fcc5bc
Yeison Danner carhuapoma Dett <51946409+yeisondanner@users.noreply.github.com>
2025-12-24 00:42:43 -0500

    actualizacion de historial caja

--- Views/App/POS/Boxhistory/boxhistory.php
+++ Views/App/POS/Boxhistory/boxhistory.php
@@ -75,6 +75,28 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-12">
+            <div class="tile rounded-3">
+                <div class="tile-body">
+                    <div class="table-responsive table-responsive-sm bg-light rounded-3 border p-1">
+                        <table class="table table-sm table-hover table-bordered table-striped table-responsive" id="table" data-token="<?= csrf(false); ?>">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Acciones</th>
+                                    <th>Usuario</th>
+                                    <th>Valor</th>
+                                    <th>Medios de Pago</th>
+                                    <th>Nombre de Vendedor</th>
+                                    <th>Fecha y Hora</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 <?= footerPos($data) ?>
