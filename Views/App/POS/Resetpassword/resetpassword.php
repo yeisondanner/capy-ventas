14475e34b6a6558c73fac19a6001d68969e7aa04
Yeison Danner carhuapoma Dett <51946409+yeisondanner@users.noreply.github.com>
2026-03-12 17:26:12 -0500

    correccion de librerias a resetpaswword

--- Views/App/POS/Resetpassword/resetpassword.php
+++ Views/App/POS/Resetpassword/resetpassword.php
@@ -9,9 +9,12 @@
     <link rel="stylesheet" type="text/css" href="<?= media() ?>/css/libraries/POS/main.css?<?= versionSystem() ?>">
     <link rel="shortcut icon" href="<?= media() ?>/capysm.png?<?= versionSystem() ?>" type="image/x-icon">
     <!-- Font-icon css-->
-    <link rel="stylesheet" href="<?= media() ?>/css/libraries/POS/bootstrap-icons.min.css?<?= versionSystem() ?>">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/css/libraries/Admin/toastr.min.css?<?= versionSystem() ?>">
-    <link rel="stylesheet" type="text/css" href="<?= media() ?>/css/app/POS/account/style_account.css?<?= versionSystem() ?>">
+    <link rel="stylesheet"
+        href="<?= media() ?>/css/libraries/POS/plugins/Bootstrapicons/v1.13.1/bootstrap-icons.min.css?<?= versionSystem() ?>">
+    <link rel="stylesheet" type="text/css"
+        href="<?= media() ?>/css/libraries/Admin/toastr.min.css?<?= versionSystem() ?>">
+    <link rel="stylesheet" type="text/css"
+        href="<?= media() ?>/css/app/POS/account/style_account.css?<?= versionSystem() ?>">
     <title>Recuperar contraseña | Capy Ventas</title>
     <script>
         const base_url = "<?= base_url() ?>/pos";
@@ -26,23 +29,32 @@
     </section>
 
     <section class="d-flex justify-content-center align-items-center min-vh-100 p-3 pt-4 pt-md-3">
-        <div id="cardAccount" class="card shadow-lg border-0 overflow-hidden w-100 rounded-3" style="max-width: 1000px;">
+        <div id="cardAccount" class="card shadow-lg border-0 overflow-hidden w-100 rounded-3"
+            style="max-width: 1000px;">
 
         </div>
     </section>
 
-    <a target="_blank" href="https://wa.me/51910367611?text=Hola,%20tengo%20problemas%20con%20la%20recuperación%20de%20contraseña%20en%20Capy%20Ventas" class="btn-whatsapp-float">
+    <a target="_blank"
+        href="https://wa.me/51910367611?text=Hola,%20tengo%20problemas%20con%20la%20recuperación%20de%20contraseña%20en%20Capy%20Ventas"
+        class="btn-whatsapp-float">
         <i class="bi bi-whatsapp me-2"></i> ¿Tienes problemas con la recuperación?
     </a>
     <!-- TODO: Essential javascripts for application to work-->
-    <script src="<?= media() ?>/js/libraries/POS/plugins/jquery/v3.7.0/jquery-3.7.0.min.js?<?= versionSystem() ?>"></script>
-    <script src="<?= media() ?>/js/libraries/POS/popper.min.js?<?= versionSystem() ?>"></script>
-    <script src="<?= media() ?>/js/libraries/POS/bootstrap.min.js?<?= versionSystem() ?>"></script>
-    <script src="<?= media() ?>/js/libraries/POS/plugins/SweetAlert2/v11.26.3/SweerAlert2.js?<?= versionSystem() ?>"></script>
-    <script src="<?= media() ?>/js/libraries/POS/main.js?<?= versionSystem() ?>"></script>
-    <script type="module" src="<?= media() ?>/js/app/POS/resetpassword/functions_resetpassword_api.js?<?= versionSystem() ?>"></script>
-    <script type="module" src="<?= media() ?>/js/app/POS/resetpassword/functions_resetpassword.js?<?= versionSystem() ?>"></script>
-    <script type="module" src="<?= media() ?>/js/app/POS/<?= strtolower($data["page_container"]) ?>/functions_<?= $data["page_js_css"] ?>.js?<?= versionSystem() ?>"></script>
+    <script type="text/javascript" language="javascript"
+        src="<?= media() ?>/js/libraries/POS/plugins/Jquery/v3.7.0/jquery-3.7.0.min.js?<?= versionSystem() ?>"></script>
+    <script type="text/javascript" language="javascript"
+        src="<?= media() ?>/js/libraries/POS/popper.min.js?<?= versionSystem() ?>"></script>
+    <script type="text/javascript" language="javascript"
+        src="<?= media() ?>/js/libraries/POS/bootstrap.min.js?<?= versionSystem() ?>"></script>
+    <script type="text/javascript" language="javascript"
+        src="<?= media() ?>/js/libraries/POS/plugins/SweetAlert2/v11.26.3/SweerAlert2.js?<?= versionSystem() ?>"></script>
+    <script type="text/javascript" language="javascript"
+        src="<?= media() ?>/js/libraries/POS/main.js?<?= versionSystem() ?>"></script>
+    <script type="module"
+        src="<?= media() ?>/js/app/POS/resetpassword/functions_resetpassword_api.js?<?= versionSystem() ?>"></script>
+    <script type="module"
+        src="<?= media() ?>/js/app/POS/resetpassword/functions_resetpassword.js?<?= versionSystem() ?>"></script>
 </body>
 
 </html>
